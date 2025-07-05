78d1c8037d0917b225cd99f2029f31e097dca830
curi57 <shopper.tiago@gmail.com>
2025-07-04 23:24:25 -0300

    chore: 1 test missing

--- shopping.py
+++ shopping.py
@@ -3,24 +3,21 @@ import sys
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.utils.estimator_checks import check_no_attributes_set_in_init
 
 TEST_SIZE = 0.4
 
-
+# check50 ai50/projects/2024/x/shopping
 def main():
 
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
 
-    # Load data from spreadsheet and split into train and test sets
     evidence, labels = load_data(sys.argv[1])
-    X_train, X_test, y_train, y_test = train_test_split(
-        evidence, labels, test_size=TEST_SIZE
-    )
 
-    # Train model and make predictions
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE)
+
     model = train_model(X_train, y_train)
     predictions = model.predict(X_test)
     sensitivity, specificity = evaluate(y_test, predictions)
@@ -31,48 +28,57 @@ def main():
     print(f"True Positive Rate: {100 * sensitivity:.2f}%")
     print(f"True Negative Rate: {100 * specificity:.2f}%")
 
-
 def load_data(filename):
     with open('shopping.csv', 'r', newline='\n') as ocurrences_csv:
         ocurrences = csv.reader(ocurrences_csv, delimiter=',')
 
-        data_labels : tuple[list[list], int] = []
+        data_labels : tuple[list[list], [int]] = ([], [])
         for features in ocurrences:
-            print(features)
+            if features[0] == "Administrative":
+                continue
 
-            data = [].append(
-                [
-                    int(features[0]),
-                    float(features[1]),
-                    int(features[2]),
-                    float(features[3]),
-                    int(features[4]),
-                    float(features[5]),
-                    float(features[6]),
-                    float(features[7]),
-                    float(features[8]),
-                    get_month(features[9]),
-                    int(features[10]),
-                    int(features[11]),
-                    int(features[12]),
-                    int(features[13]),
-                    int(features[14])
-                ])
+            input = [
+                int(features[0]), # Administrative,
+                float(features[1]), # Administrative_Duration,
+                int(features[2]), # Informational,
+                float(features[3]), # Informational_Duration,
+                int(features[4]), # ProductRelated,
+                float(features[5]), # ProductRelated_Duration,
+                float(features[6]), # BounceRates,
+                float(features[7]), # ExitRates,
+                float(features[8]), # PageValues,
+                float(features[9]), # SpecialDay,
+                get_month(features[10]), # Month,
+                int(features[11]), # OperatingSystems,
+                int(features[12]), # Browser,
+                int(features[13]), # Region,
+                int(features[14]), # TrafficType,
+                int(get_visitor(features[15])), # VisitorType,
+                1 if features[16] == "TRUE" else 0  # Weekend,
+            ]
 
-            data_labels[0].append(data)
-            data_labels[1] = int(features[15])
+            data, labels = data_labels
+            labels.append(1 if features[17] == "TRUE" else 0)
+            data.append(input)
+
+
+        #print_evidence, print_labels = data_labels
+        #print(f"evidence: {print_evidence}")
+        #print(f"labels: {print_labels}")
 
         return data_labels
 
-
-
 def get_month(month : str):
     return {
-        "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "June": 5,
-        "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11 }.get(month)
+        "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3,
+        "May": 4, "June": 5, "Jul": 6, "Aug": 7,
+        "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11 }.get(month)
+
+def get_visitor(visitor : str):
+    return {
+        "Returning_Visitor": 1, "New_Visitor": 0, "Other": 0 }.get(visitor)
 
 def train_model(evidence, labels):
-
     neigh = KNeighborsClassifier(n_neighbors=1)
     neigh.fit(evidence, labels)
 
@@ -81,21 +87,34 @@ def train_model(evidence, labels):
     return neigh
 
 def evaluate(labels, predictions):
-    """
-    Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificity).
 
-    Assume each label is either a 1 (positive) or 0 (negative).
+    assert len(labels) == len(predictions)
 
-    `sensitivity` should be a floating-point value from 0 to 1
-    representing the "true positive rate": the proportion of
-    actual positive labels that were accurately identified.
+    zipped = zip(labels, predictions)
+    #print(list(zipped))
 
-    `specificity` should be a floating-point value from 0 to 1
-    representing the "true negative rate": the proportion of
-    actual negative labels that were accurately identified.
-    """
-    raise NotImplementedError
+    sensitivity = 0
+    specificity = 0
+    counter = 1
+    for label, prediction in list(zipped):
+        if label == 1:
+            if prediction == 1:
+                sensitivity += (sensitivity + 1) / counter
+            else:
+                sensitivity = sensitivity / counter
+        elif label == 0:
+            if prediction == 0:
+                specificity += (specificity + 1) / counter
+            else:
+                specificity = specificity / counter
+        counter += 1
+
+    return sensitivity, specificity
+
+
+
+
+
 
 
 if __name__ == "__main__":
